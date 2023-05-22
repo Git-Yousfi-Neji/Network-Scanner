90c1f2718246f7538e28c926d11d3adfa53b8581
adel <adel.bouabidi@sagemcom.com>
2023-05-22 16:00:36 +0100

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -8,34 +8,51 @@
 #include <netdb.h>
 #include "scanner.h"
 
-void performScan(const char* ipRange, int ipStart, int ipEnd) {
-    printf("in scanner");
-    struct sockaddr_in server;
-    int sock;
-    int port;
-    int result;
-
-    for (port = ipStart; port <= ipEnd; ++port) {
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        printf("in loop");
+void performScan(const char* ipRange, int startPort, int endPort) {
+    // Iterate over the specified port range
+    for (int port = startPort; port <= endPort; ++port) {
+        // Create a socket
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
         if (sock == -1) {
-            printf("get error");
-            perror("socket");
-            exit(EXIT_FAILURE);
+            perror("Socket creation failed");
+            continue;
         }
-        printf("good");
+
+        // Prepare the server address structure
+        struct sockaddr_in server;
         server.sin_family = AF_INET;
-        server.sin_port = htons(port);
-        if (inet_pton(AF_INET, ipRange, &(server.sin_addr)) <= 0) {
-            perror("inet_pton");
-            exit(EXIT_FAILURE);
+
+        // Iterate over the IP addresses in the range
+        char ip[16];  // Buffer to store the current IP address
+        strncpy(ip, ipRange, sizeof(ip));
+        char* hyphen = strchr(ip, '-');
+        if (hyphen != NULL) {
+            *hyphen = '\0';
         }
 
-        result = connect(sock, (struct sockaddr*)&server, sizeof(server));
-        if (result == 0) {
-            printf("Port %d is open\n", port);
+        struct in_addr addr;
+        if (inet_pton(AF_INET, ip, &addr) != 1) {
+            perror("Invalid IP address");
+            close(sock);
+            continue;
+        }
+        
+
+        // Scan the IP range
+        for (uint32_t i = ntohl(addr.s_addr); i <= ntohl(server.sin_addr.s_addr); ++i) {
+            server.sin_addr.s_addr = htonl(i);
+
+            // Set the port number
+            server.sin_port = htons(port);
+
+            // Attempt to connect to the IP address and port
+            if (connect(sock, (struct sockaddr*)&server, sizeof(server)) == 0) {
+                // Connection successful, port is open
+                printf("Open port found on %s:%d\n", inet_ntoa(server.sin_addr), port);
+            }
         }
 
+        // Close the socket
         close(sock);
     }
 }
