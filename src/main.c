6b9cf934568b044358538fc6dc4fc0acd18bdc3e
root <yousfineji1@yahoo.com>
2023-05-22 22:43:42 +0000

    Updates

--- src/main.c
+++ src/main.c
@@ -2,19 +2,20 @@
 #include "scanner.h"
 
 int main() {
-    char ipRange[20];
-    int startPort, endPort;
+    char ipRange[100];
+    int startPort=79, endPort=81;
 
     printf("Enter IP range to scan: ");
-    scanf("%19s", ipRange);
-    
+    //scanf("%26s", ipRange);
+    /*
     printf("Enter IP Start: ");
     scanf("%d", &startPort);
     
     printf("Enter IP End: ");
     scanf("%d", &endPort);
-
-    performScan(ipRange, startPort, endPort);
+    */
+    printf("ip=%s\n", ipRange) ;
+    performScan("192.168.1.1-192.168.1.2", startPort, endPort);
     printf("Scan Completed.\n");
 
     return 0;
--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -2,17 +2,46 @@
 #include "scanner.h"
 #include "scanner_internal.h"
 #include "../utils/ip_range.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
 
 bool scanPorts(unsigned int ipAddress, int port) {
-    // Implement your port scanning logic here
-    // You can use system calls, sockets, or any other networking library to perform the port scan
+    // Create a socket for the port scan
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return false;
+    }
 
-    // Placeholder code
-    // Simulate a random port being open
-    bool isOpen = (rand() % 2 == 0);
+    // Prepare the socket address structure
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(ipAddress);
+    serverAddress.sin_port = htons(port);
 
-    return isOpen;
-}
+    // Attempt to connect to the specified port
+    int connectResult = connect(sockfd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (connectResult < 0) {
+        // Connection failed, port is closed
+        close(sockfd);
+        return false;
+    }
+
+    // Connection successful, port is open
+    close(sockfd);
+    return true;
+} 
+
+
+
+
+
 
 void performScan(const char* ipRange, int startPort, int endPort) {
     printf("Performing network scan...\n");
@@ -25,12 +54,16 @@ void performScan(const char* ipRange, int startPort, int endPort) {
     }
 
     // Iterate over the IP addresses in the range
-    for (unsigned int ip = range.start; ip <= range.end; ++ip) {
-        printf("Scanning IP: %u.%u.%u.%u\n", (ip >> 24) & 0xFF, (ip >> 16) & 0xFF, (ip >> 8) & 0xFF, ip & 0xFF);
+    unsigned int startIP = ntohl(range.start); // Convert start IP to host byte order
+    unsigned int endIP = ntohl(range.end);     // Convert end IP to host byte order
 
+    for (unsigned int ip = startIP; ip <= endIP; ++ip) {
+        unsigned int currIP = htonl(ip); // Convert IP to network byte order
+        printf("Scanning IP: %u.%u.%u.%u\n", currIP & 0xFF, (currIP >>8) & 0xFF, (currIP >> 16) & 0xFF, (currIP >> 24) & 0xFF);
         // Iterate over the ports in the range
         for (int port = startPort; port <= endPort; ++port) {
-            if (scanPorts(ip, port)) {
+            if (scanPorts(currIP, port)) {
+                printf("Trying with port: %d\n", port) ;
                 printf("Port %d open\n", port);
             }
         }
@@ -38,3 +71,4 @@ void performScan(const char* ipRange, int startPort, int endPort) {
 
     printf("Network scan completed.\n");
 }
+ 
