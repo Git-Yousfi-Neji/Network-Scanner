1f32abba81b1298714fbfa984377051a4149482b
adel <adel.bouabidi@sagemcom.com>
2023-05-23 11:28:27 +0100

    Update main function

--- src/main.c
+++ src/main.c
@@ -2,20 +2,19 @@
 #include "scanner.h"
 
 int main() {
-    //char ipRange[100];
-    int startPort=1;
-    int endPort=445;
+    char ipRange[100];
+    int startPort, endPort;
 
-    //printf("Enter IP range to scan: ");
-    //scanf("%26s", ipRange);
-    /*
+    printf("Enter IP range to scan: ");
+    scanf("%26s", ipRange);
+    
     printf("Enter IP Start: ");
     scanf("%d", &startPort);
     
     printf("Enter IP End: ");
     scanf("%d", &endPort);
-    */
-    performScan("192.168.1.1-192.168.1.3", startPort, endPort);
+
+    performScan(ipRange, startPort, endPort);
     printf("Scan Completed.\n");
 
     return 0;
