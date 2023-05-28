7cc075acd85ebec5742eef9238d0f54c65684df5
root <yousfineji1@yahoo.com>
2023-05-28 13:20:41 +0000

    New updates

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -14,82 +14,6 @@
 #include <errno.h> // Added for error handling
 
 #define VERBOSE_MODE read_config("CONFIG_VERBOSE_MODE")
-#define TIMEOUT_VALUE read_config("CONFIG_TIMEOUT_SEC")
-
-bool scanPort(const char* ipAddress, int port) {
-    // Create a socket for the port scan
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        return false;
-    }
-
-    // Prepare the socket address structure
-    struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr(ipAddress);
-    serverAddress.sin_port = htons(port);
-
-    // Set the socket to non-blocking mode
-    int flags = fcntl(sockfd, F_GETFL, 0);
-    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
-
-    // Attempt to connect to the specified port
-    int connectResult = connect(sockfd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-
-    if (connectResult < 0) {
-        // Check if the connection is still in progress
-        if (VERBOSE_MODE == 2) printf("Connection still in progress with port %d\n",port);
-        if (errno == EINPROGRESS) {
-            // Define the timeout value for the connection
-            struct timeval timeout;
-            timeout.tv_sec = TIMEOUT_VALUE;  // second
-            timeout.tv_usec = 0; // 0 microseconds
-
-            // Create a file descriptor set to hold the socket
-            fd_set fds;
-            FD_ZERO(&fds);
-            FD_SET(sockfd, &fds);
-
-            // Use the select function to check for socket activity
-            int selectResult = select(sockfd + 1, NULL, &fds, NULL, &timeout);
-
-            if (selectResult > 0) {
-                // The socket is ready for reading, indicating a successful connection
-                if (VERBOSE_MODE == 2) printf("Successful connection, the socket is ready for reading\n");
-                int socketError;
-                socklen_t socketErrorLength = sizeof(socketError);
-                getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &socketError, &socketErrorLength);
-
-                if (socketError == 0) {
-                    // Connection successful, port is open
-                    if (VERBOSE_MODE == 2) printf("Connection successful, port %d is open\n", port);
-                    close(sockfd);
-                    return true;
-                }
-            } else if (selectResult == 0) {
-                // The select function timed out, indicating a closed port
-                if (VERBOSE_MODE == 2) printf("Connection timed out with port %d [timeout config is set to %d sec]\n", port, TIMEOUT_VALUE);
-                close(sockfd);
-                return false;
-            }
-        } else {
-            // Connection failed, port is closed
-            if (VERBOSE_MODE == 2) printf("Connection failed, port is closed\n");
-            close(sockfd);
-            return false;
-        }
-    } else {
-        // Connection successful, port is open
-        if (VERBOSE_MODE == 2) printf("Connection successful, port is open\n");
-        close(sockfd);
-        return true;
-    }
-
-    // Close the socket
-    close(sockfd);
-    return false;
-}
 
 void performScan(const char* ipRange, int startPort, int endPort) {
     if (VERBOSE_MODE == 1 || VERBOSE_MODE == 2) printf("Performing network scan...\n");
@@ -113,9 +37,9 @@ void performScan(const char* ipRange, int startPort, int endPort) {
         // Iterate over the ports in the range
         for (int port = startPort; port <= endPort; ++port) {
             if (VERBOSE_MODE == 1 || VERBOSE_MODE == 2) printf("Trying with port: %d\n", port);
-            if (scanPort(ipAddress, port)) {
+            /*if (scanPort(ipAddress, port)) {
                 printf("Port %d open\n", port);
-            }
+            }*/
             scanUDPPort(ip, port);
             scanTCPPort(ip, port);
         }
